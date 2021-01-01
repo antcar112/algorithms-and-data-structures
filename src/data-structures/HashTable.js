a4f9787d2724bbfa68c25afa32955c737947a151
Anthony Caron <antcar1992@gmail.com>
2021-01-01 14:33:19 -0800

    Added hash function

--- src/data-structures/HashTable.js
+++ src/data-structures/HashTable.js
@@ -1,3 +1,25 @@
-class HashTable {}
+class HashTable {
+  constructor() {}
+
+  /**
+   * Detemines the index for a given key.
+   *
+   * @param {string} key The key to store a value by
+   * @param {number?} arrayLen How big the array should be (ideally should be a prime number)
+   * @returns {number} The index to store the value at
+   */
+  static hash(key, arrayLen = 101) {
+    const PRIME = 31
+
+    let total = 0
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      const char = key[i]
+      // map "a" to 1, "b" to 2, "c" to 3, etc.
+      const value = char.charCodeAt(0) - 96
+      total = (total * PRIME + value) % arrayLen
+    }
+    return total
+  }
+}
 
 module.exports = HashTable
--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 const { HashTable } = require('./data-structures')
 
 const table = new HashTable()
+
+const i = HashTable.hash('pink')
+const j = HashTable.hash('orange')
+const k = HashTable.hash('red')
+console.log(i, j, k)
